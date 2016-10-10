42c3b5511425c8fe1187ab0ce821211238a48d82
Chingis <csandanov@users.noreply.github.com>
2016-10-10 14:02:31 +0700

    Update wodby.php

--- scripts/wodby.php
+++ scripts/wodby.php
@@ -1,13 +1,5 @@
 <?php
-
-define('MY_ORG_ID', '9947e435-01dd-4aa7-a467-fb1567c19537');
-// Server where to deploy the new instance.
-define('MY_SERVER_ID', 'f8b23aae-079b-4fb5-916c-e095702f46ed');
-// Application to deploy.
-define('MY_APP_ID', 'ba42c254-264a-4295-96f4-68516a658a01');
-// Use this instance to copy db and files.
-define('MY_APP_SOURCE_INSTANCE_ID', 'a6573194-8043-488c-ba20-0c8fd3547f4d');
-
+  
 use \Wodby\Api\Entity;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -17,16 +9,15 @@ $api = new Wodby\Api($_SERVER['WODBY_API_TOKEN'], new GuzzleHttp\Client());
 echo PHP_EOL;
 echo "Creating instance.", PHP_EOL;
 $result = $api->instance()->create(
-  MY_APP_ID,
+  $_SERVER['WODBY_APP_ID'],
   'test-' . $_SERVER['CIRCLE_BUILD_NUM'],
   Entity\Instance::TYPE_STAGE,
-  $_SERVER['CIRCLE_BRANCH'],
-  MY_SERVER_ID,
-  "CircleCI build {$_SERVER['CIRCLE_BUILD_NUM']}",
+  $_SERVER['WODBY_SERVER_ID'],
   [
-    Entity\Instance::COMPONENT_DATABASE => MY_APP_SOURCE_INSTANCE_ID,
-    Entity\Instance::COMPONENT_FILES => MY_APP_SOURCE_INSTANCE_ID,
-  ]
+    Entity\Instance::COMPONENT_DATABASE => $_SERVER['WODBY_SOURCE_INSTANCE_ID'],
+    Entity\Instance::COMPONENT_FILES => $_SERVER['WODBY_SOURCE_INSTANCE_ID'],
+  ],
+  "[CircleCI] Test Build {$_SERVER['CIRCLE_BUILD_NUM']}"
 );
 
 /** @var Entity\Task $task */
@@ -38,18 +29,35 @@ $instance = $result['instance'];
 echo "Deploying new instance via Wodby...", PHP_EOL;
 $api->task()->wait($task->getId(), 600);
 
-echo "Reload instance", PHP_EOL;
+echo "Reload instance variable", PHP_EOL;
 $instance = $api->instance()->load($instance->getId());
 
-echo "Done!", PHP_EOL;
-var_dump($instance);
+// Assemble build artifacts URL.
+$s3_bucket = $_SERVER['WODBY_AWS_S3_BUCKET'];
+$s3_file_name = $_SERVER['CIRCLE_PROJECT_USERNAME'] . '-' . $_SERVER['CIRCLE_SHA1'];
+$build_file_url = "https://s3.amazonaws.com/$s3_bucket/$s3_file_name.tar.gz";
 
-echo "Importing build", PHP_EOL;
-$result = $api->instance()
-  ->importCodebase($instance->getId(), $_SERVER['CIRCLE_BUILD_URL'] . '/build.tar.gz');
+echo "Importing $build_file_url", PHP_EOL;
+$result = $api->instance()->importCodebase($instance->getId(), $build_file_url);
 
 /** @var Entity\Task $task */
 $task = $result['task'];
 $api->task()->wait($task->getId(), 600);
+
+echo "Updating build info", PHP_EOL;
+$api->instance()->updateProperty(
+  $instance->getId(),
+  Entity\Instance::PROPERTY_BUILD_INFO,
+  [
+    'builder' => 'CircleCI',
+    'project_name' => $_SERVER['CIRCLE_PROJECT_USERNAME'],
+    'project_url' => $_SERVER['CIRCLE_REPOSITORY_URL'],
+    'build_id' => $_SERVER['CIRCLE_BUILD_NUM'],
+    'build_url' => $_SERVER['CIRCLE_BUILD_URL'],
+    'build_download_url' => $build_file_url,
+    'git_branch' => $_SERVER['CIRCLE_BRANCH'],
+    'git_commit' => $_SERVER['CIRCLE_SHA1'],
+  ]
+);
 
 echo "Done!", PHP_EOL;
