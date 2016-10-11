1be2bb16266cf8efad1dfbac2a475fd917c1815e
Chingis Sandanov <chingis@wodby.com>
2016-10-11 17:13:51 +0700

    run circleci tests via docker

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,7 +4,8 @@ set -eo pipefail
 composer global require "hirak/prestissimo:^0.3"
 composer require "wikimedia/composer-merge-plugin:~1.3" --no-interaction
 composer update --no-interaction
-vendor/bin/phpunit core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
-vendor/bin/phpunit core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
-vendor/bin/phpunit core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
-vendor/bin/phpunit core/tests/Drupal/FunctionalJavascriptTests/Core/Session/SessionTest.php
+cd core
+phpunit core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
+phpunit core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
+../vendor/bin/phpunit core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
+../vendor/bin/phpunit core/tests/Drupal/FunctionalJavascriptTests/Core/Session/SessionTest.php
