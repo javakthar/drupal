7352b6eb72a532ffb8c6bcf176a1597c573f7cfd
Chingis Sandanov <chingis@wodby.com>
2016-10-11 13:55:31 +0700

    run circleci tests via docker

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+composer global require "hirak/prestissimo:^0.3"
+composer require "wikimedia/composer-merge-plugin:~1.3" --no-interaction
+composer update --no-interaction
 drush si standard --y --account-name=admin --account-pass=admin --db-url=mysql://drupal:drupal@mariadb:3306/drupal
 php ./core/scripts/run-tests.sh --url http://localhost:8000/ --php /usr/bin/php --dburl mysql://drupal:drupal@mariadb:3306/drupal 'Drupal\Tests\Component\Utility\ArgumentsResolverTest'
 php ./core/scripts/run-tests.sh --php /usr/bin/php PHPUnit
