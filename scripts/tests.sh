1a2477e230357d07d302bef2f8491fcbef4d2026
Chingis Sandanov <chingis@wodby.com>
2016-10-11 16:30:11 +0700

    run circleci tests via docker

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -3,6 +3,7 @@
 composer global require "hirak/prestissimo:^0.3"
 composer require "wikimedia/composer-merge-plugin:~1.3" --no-interaction
 composer update --no-interaction
+drush si standard --y --account-name=admin --account-pass=admin --db-url=mysql://drupal:drupal@mariadb:3306/drupal
 cd core
-../vendor/bin/phpunit --testsuite=unit
-../vendor/bin/phpunit --testsuite=kernel
+../vendor/bin/phpunit tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
+../vendor/bin/phpunit tests/Drupal/FunctionalJavascriptTests/Core/Session/SessionTest.php
