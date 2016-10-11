8ea6c1f65957a0a2386e8d2b312a54662fd1ec7b
Chingis Sandanov <chingis@wodby.com>
2016-10-11 17:39:17 +0700

    run circleci tests via docker

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,7 +6,6 @@ composer require "wikimedia/composer-merge-plugin:~1.3" --no-interaction
 composer update --no-interaction
 cd core
 ../vendor/bin/phpunit tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
-../vendor/bin/phpunit tests/Drupal/Tests/Component/Utility/HtmlTest.php
 ../vendor/bin/phpunit tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
 ../vendor/bin/phpunit tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
 ../vendor/bin/phpunit tests/Drupal/FunctionalJavascriptTests/Core/Session/SessionTest.php
